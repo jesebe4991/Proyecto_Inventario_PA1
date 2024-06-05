083c68c2f8429d1b970dea0b8fea15805a63fde1
Jean <jean.benitez@mawesi.net>
2024-06-04 20:06:20 -0500

    Ajuste archivos definitivos y eliminacion de registro

--- opt/producto.py
+++
@@ -1,7 +0,0 @@
-class Producto:
-    def __init__(self, nombre, descripcion, precio, stok_inicial, categoria):
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.precio = precio
-        self.stok_inicial = stok_inicial
-        self.categoria = categoria
